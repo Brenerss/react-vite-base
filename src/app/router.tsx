7c7002b5505e3f5b225642afdc73de778c829991
brenerss <brener.dev@gmail.com>
2026-08-13 21:30:52 -0300

    refactor: simplify router implementation by removing unused code and consolidating routing logic

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,36 +1,12 @@
-import { type QueryClient, useQueryClient } from "@tanstack/react-query";
-import { useMemo } from "react";
-import { type ActionFunction, type LoaderFunction, createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Home from "./routes/home";
 
-type RouteModule = {
-  default: React.ComponentType;
-  clientLoader?: (queryClient: QueryClient) => LoaderFunction;
-  clientAction?: (queryClient: QueryClient) => ActionFunction;
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+]);
 
-const convert = (queryClient: QueryClient) => (m: RouteModule) => {
-  const { clientLoader, clientAction, default: Component, ...rest } = m;
-  return {
-    ...rest,
-    loader: clientLoader?.(queryClient),
-    action: clientAction?.(queryClient),
-    Component,
-  };
-};
-
-export const createAppRouter = (queryClient: QueryClient) =>
-  createBrowserRouter([
-    {
-      path: "/",
-      lazy: () => import("./routes/home").then(convert(queryClient)),
-    },
-  ]);
-
-export const AppRouter = () => {
-  const queryClient = useQueryClient();
-
-  const router = useMemo(() => createAppRouter(queryClient), [queryClient]);
-
-  return <RouterProvider router={router} />;
-};
+export const AppRouter = () => <RouterProvider router={router} />;
